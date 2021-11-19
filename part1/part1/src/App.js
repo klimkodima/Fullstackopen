b72542274d8cf3d36b8c1e379c378977576c2e33
klimkodima <klimkodima@tut.by>
2021-11-19 13:38:07 +0300

    part1.5

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   const Header = (props) => {
     return (
@@ -39,9 +40,9 @@ const App = () => {
   const Content = (props) => {
     return (
       <>
-      <Part part={parts[0]}/>
-      <Part part={parts[1]}/>
-      <Part part={parts[2]}/>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
       </>
     )
   }
@@ -54,9 +55,9 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total  parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total  parts={course.parts}/>
     </div>
   )
 }
