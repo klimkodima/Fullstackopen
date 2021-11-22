5a96cb8458259a5671f01449a3676c1ca26626ad
klimkodima <klimkodima@tut.by>
2021-11-22 11:00:10 +0300

    part2.4

--- part2/course/src/App.js
+++ part2/course/src/App.js
@@ -1,32 +1,50 @@
 import React from 'react'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   const Header = ({name}) => {
     return (
       <div>
@@ -71,6 +89,10 @@ const App = () => {
     <h4>total of {total} exercises</h4>
     )
   }
-  return <Course course={course}/>
+  return (
+    <ul>
+     {courses.map(course => <Course key={course.id} course={course}/>)}
+    </ul>
+  )
 }
 export default App
